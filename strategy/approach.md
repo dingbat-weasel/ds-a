83d441e496e228a969120a0cb8fe74289d403513
dingbat-weasel <isaacrperk@gmail.com>
2023-11-04 22:46:16 -0700

    frequency counter strategy notes and examples

---
+++ strategy/approach.md
@@ -0,0 +1,33 @@
+# Approach to Solving Problems
+
+1. Understand the Problem
+   1. Can I restate the problem in my own words?
+   2. What are the inputs that go into the problem?
+   3. What are the outputs that should come from the solution to the problem?
+   4. Can the outputs be determined from the inputs?
+      1. In other words, do I have enough information to solve the problem?
+   5. How should I label the important pieces of data that are a part of the problem?
+2. Explore Concrete Examples
+   1. Start with Simple Examples
+   2. Progress to More Complex Examples
+   3. Explore Examples with Empty Inputs
+   4. Explore Examples with Invalid Inputs
+3. Break it Down
+   1. Explicitly write out the steps you need to take
+      1. This forces you to think about your code before you write it
+      2. Helps you catch any lingering conceptual issues or misunderstandings
+4. Solve/Simplify
+   1. Solve the problem
+   2. If you can’t, solve a simpler problem
+      1. Find the core difficulty in what you’re trying to do
+      2. Temporarily ignore that difficulty
+      3. Write a simplified solution
+      4. Then incorporate that difficulty back in
+5. Look Back and Refactor
+   1. Can you check the result?
+   2. Can you derive the result differently?
+   3. Can you understand it at a glance?
+   4. Can you use the result or method for some other problem?
+   5. Can you improve the performance of your solution?
+   6. Can you think of other ways to refactor?
+   7. How have other people solved this problem?
---
+++ strategy/frequency_counters.js
@@ -0,0 +1,119 @@
+/*
+- This pattern uses objects or sets to collect values/frequencies of values
+- This can often avoid the need for nested loops or O(N^2) operations with arrays/strings
+- Example:
+    - Write a function called 'same', which accepts two arrays. The function should return true if every value in the array has its corresponding value squared in the second array. The frequency of values must be the same.
+*/
+
+// Naive example; nested for loops run in O(n^2) time
+function nestedSame(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    let correctIndex = arr2.indexOf(arr1[i] ** 2);
+    if (correctIndex === -1) return false;
+    arr2.splice(correctIndex, 1);
+  }
+  return true;
+}
+
+// Parallel for loops run in O(n) time
+function same(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+
+  // objects each count the frequency of individual values in the arrays
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+  // for each value in array count the frequency of the value
+  for (let val of arr1) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+  }
+  for (let val of arr2) {
+    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+  }
+  // check that val^2 occurs in arr2 in equivalent frequency for each value
+  for (let key in frequencyCounter1) {
+    // does the square occur at all?
+    if (!(key ** 2 in frequencyCounter2)) return false;
+    // does it occur often enough?
+    if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) return false;
+  }
+  return true;
+}
+
+/*
+Anagrams
+Given two strings, write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
+*/
+
+function validAnagram(phrase1, phrase2) {
+  // clean str in case of spaces
+  const str1 = phrase1.split(' ').join('');
+  const str2 = phrase2.split(' ').join('');
+  // if str1 and str2 are different lengths, return false
+  if (str1.length !== str2.length) {
+    console.log(false);
+    return false;
+  }
+  // create two empty frequency counter objects
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+  // loop over each str, creating a key value pair in obj for the letter and the frequency counter
+  for (let val in str1) {
+    if (!frequencyCounter1[str1[val]]) {
+      frequencyCounter1[str1[val]] = 1;
+    } else {
+      frequencyCounter1[str1[val]] = frequencyCounter1[str1[val]] + 1;
+    }
+    console.log(frequencyCounter1);
+  }
+  for (let val in str2) {
+    if (!frequencyCounter2[str2[val]]) {
+      frequencyCounter2[str2[val]] = 1;
+    } else {
+      frequencyCounter2[str2[val]] = frequencyCounter2[str2[val]] + 1;
+    }
+    console.log(frequencyCounter2);
+  }
+  for (let key in frequencyCounter1) {
+    // check if each letter in str1 exists in str2
+    if (!key in frequencyCounter2) {
+      console.log(false);
+      return false;
+    }
+    // check that it exists the correct number of times
+    if (frequencyCounter1[key] !== frequencyCounter2[key]) {
+      console.log(false);
+      return false;
+    }
+  }
+  console.log(frequencyCounter1, frequencyCounter2);
+  console.log(true);
+  return true;
+}
+
+// validAnagram('a decimal point', 'im a dot in place');
+
+// Refactored validAnagram
+function validAnagram2(str1, str2) {
+  if (str1.length !== str2.length) return false;
+
+  const lookup = {};
+  for (let i = 0; i < str1.length; i++) {
+    let letter = str1[i];
+    lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
+  }
+  for (let i = 0; i < str2.length; i++) {
+    let letter = str2[i];
+    if (!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= 1;
+    }
+  }
+  console.log(true);
+  return true;
+}
+
+validAnagram2('cat', 'tac');
