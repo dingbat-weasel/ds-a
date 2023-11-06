f65b884c849b6406bd08f50e0ee649fc0c51fbb7
dingbat-weasel <isaacrperk@gmail.com>
2023-11-05 16:56:47 -0800

    multiple pointers, marching same direction

--- strategy/multiple_pointers.js
+++ strategy/multiple_pointers.js
@@ -44,4 +44,17 @@ function sumZero(arr) {
 /*
 Implement a function called countUniqueValues, which accepts a sorted array, and counts the unique values in the array. There can be negative numbers in the array, but it will always be sorted.
 */
-function countUniqueValues() {}
+
+function countUniqueValues(arr) {
+  let i = 0;
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+}
+
+// const arr = [1, 1, 2, 3, 3, 4, 5, 6, 6, 7];
+// console.log(countUniqueValues(arr));
